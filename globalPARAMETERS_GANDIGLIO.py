c96029b6a3cefca1f9b96628d408aaca43ef86e3
lorenzobruno321 <131887938+lorenzobruno321@users.noreply.github.com>
2023-08-30 16:06:34 +0200

    Add files via upload

---
+++ globalPARAMETERS_GANDIGLIO.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 29 17:17:23 2023
+
+@author: bruno
+"""
+
+import pandas
+from scipy.io import loadmat
+
+#====================================================================
+## TIME DATA for the code
+#====================================================================
+time_end = 8760                                                                      # [h]
+time_vec = list(range(0, time_end))
+
+mat= loadmat('thermalload_momo_new.mat')
+mat= loadmat('PL.mat')
+PL = mat['PL']
+PL = PL.reshape((-1))
+PL = PL[0:time_end]
+
+efficiency_ele = 0.65  
+power_EL_rated_min = 0
+power_EL_rated_max = 1e6
+
+specific_work_cp = 4                                                                    # [MJ/kgH2]
+compression_work = specific_work_cp * 1000 / 3600                                       # [kWh/kgH2] = [MJ/kgH2] * [kWh/MJ] 
+LHV_h2 = 33.33                                                                             # [kWh/kg]
+
+capacity_HT_rated = 1000            #[kWh]
+perc_min_ht = 10            #assumed
+perc_max_ht = 90            #assumed
+
+efficiency_bur = 98
+perc_min_bur = 0
+perc_max_bur = 1
+power_bur_rated = 1e6       #assumed
+
+LHV_h2 = 33.33                                                                             # [kWh/kg]
+density_h2 = 0.0899                                                                     # [kg/m3]
+density_h2o = 1000                                                                    # [kg/m3]
+ECI = 166         
+#====================================================================
+## ELECTROLYSER
+#====================================================================
+CAPEX_ele = 1188                                                                        # [€/kWe/year]  https://www.iea.org/reports/electrolysers + Marocco Gandiglio
+OPEX_ele = 15.84                                                                        # [€/kWe/year]  Marocco Gandiglio
+INSTALL_ele = CAPEX_ele*0.1                                                             # [€/kWe/year]  Marocco Gandiglio
+REPLACE_ele = CAPEX_ele*0.35                                                            # [€/kWe/year]  Marocco Gandiglio
+#====================================================================
+## HYDROGEN COMPRESSOR
+#====================================================================
+specific_work_cp = 4                                                                    # [MJ/kgH2]
+compression_work = specific_work_cp * 1000 / 3600                                       # [kWh/kgH2] = [MJ/kgH2] * [kWh/MJ] 
+CAPEX_cp = 1600                                                                         # [€/kWe/year]  Marocco Gandiglio
+OPEX_cp_USD = 19                                                                        # [USD/kW] https://emp.lbl.gov/publications/benchmarking-utility-scale-pv
+OPEX_cp = OPEX_cp_USD / 0.92                                                            # [€/kWe]
+#====================================================================
+## HYDROGEN STORAGE TANK
+#====================================================================
+loh_ht = 0.75
+perc_max_ht = 0.9                                                                       # [-] [MOMO]
+perc_min_ht = 0.1                                                                       # [-] [MOMO]
+capacity_ht_rated = 11.2e3                                                              # [kWh]             A CASOO     https://core.ac.uk/download/pdf/11653831.pdf
+CAPEX_ht = 470                                                                          # [€/kgH2/year]  Marocco Gandiglio
+OPEX_ht = OPEX_ele*0.02                                                                 # [€/kgH2/year]  Marocco Gandiglio
+#====================================================================
+## HYDROGEN BOTTLE TANK
+#====================================================================
+capacity_volume_bo = 850                                                                # [liters] https://www.mahytec.com/wp-content/uploads/2021/03/CL-DS10-Data-sheet-60bar-850L-EN.pdf
+capacity_bo_rated = capacity_volume_bo / 1000 * density_h2 * LHV_h2                        # [kWh] = [litri] * [m3/l] * [kg/m3] * [kWh/kg]
+CAPEX_bo = 470/2                                                                        # [€/kgH2/year] like the storage: MOMO said to reduce it so it is dividd by two
+OPEX_bo = OPEX_ele*0.02                                                                 # [€/kgH2/year] like the storage:
+#====================================================================
+## BURNER
+#====================================================================
+efficiency_bur = 0.98                                                                   # [-] Marocco Gandiglio
+perc_max_bur = 1                                                                        # [-] Marocco Gandiglio
+perc_min_bur = 0                                                                        # [-] Marocco Gandiglio
+power_bur_rated = 1000                                                                  # [kW] a caso (?)
+CAPEX_bur = 63.32                                                                       # [€/kWth/year]  Marocco Gandiglio
+OPEX_bur = CAPEX_bur*0.05      
---
+++ main_GANDIGLIO.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 29 17:12:16 2023
+
+@author: bruno
+"""
+import globalPARAMETERS_GANDIGLIO as glo
+import pyomo.environ as pyomo
+import matplotlib.pyplot as plt
+
+m = pyomo.ConcreteModel()
+
+m.t = pyomo.Set(initialize=range(0, glo.time_end))
+m.dt = pyomo.Set(initialize=range(0, glo.time_end+1))
+
+m.list_load_furnace = pyomo.Param(m.t, initialize=glo.PL)
+
+m.power_EG_buy = pyomo.Var(m.t, domain=pyomo.Reals, initialize=0)
+m.power_EL_in = pyomo.Var(m.t, domain=pyomo.Reals, initialize=0)
+m.power_CP_in = pyomo.Var(m.t, domain=pyomo.Reals, initialize=0)
+
+m.power_EL_out = pyomo.Var(m.t, domain=pyomo.Reals, initialize=0)
+m.power_EL_out_GB = pyomo.Var(m.t, domain=pyomo.Reals, initialize=0)
+m.power_EL_out_CP = pyomo.Var(m.t, domain=pyomo.Reals, initialize=0)
+
+m.power_GB_in = pyomo.Var(m.t, domain=pyomo.Reals, initialize=0)
+m.power_GB_out = pyomo.Var(m.t, domain=pyomo.Reals, initialize=0)
+m.power_GG_buy = pyomo.Var(m.t, domain=pyomo.Reals, initialize=0)
+m.power_HT_in = pyomo.Var(m.t, domain=pyomo.Reals, initialize=0)
+m.power_HT_out = pyomo.Var(m.t, domain=pyomo.Reals, initialize=0)
+
+m.power_EL_rated_aux = pyomo.Var(m.t, domain=pyomo.Reals, initialize=0)
+m.power_EL_rated = pyomo.Var(m.t, domain=pyomo.Reals, initialize=0)
+m.optDELTA_ele = pyomo.Var(m.t, domain=pyomo.Reals, initialize=0)
+
+m.capacity_HT = pyomo.Var(m.dt, domain=pyomo.Reals, initialize=0)
+
+
+
+m.constraints = pyomo.ConstraintList()
+
+for k in list(range(0, glo.time_end)):
+        
+    # =========================================================================
+    ## eq1: POWER BALANCE NODE 1
+    # ==========================================================================
+    m.constraints.add(m.power_EG_buy[k] == m.power_EL_in[k] + m.power_CP_in[k])
+    # =========================================================================
+    ## eq2: POWER BALANCE NODE 2
+    # ==========================================================================
+    m.constraints.add(m.power_EL_out[k] == m.power_EL_out_GB[k] + m.power_EL_out_CP[k])
+    # =========================================================================
+    ## eq3: POWER BALANCE NODE 3
+    # ==========================================================================
+    m.constraints.add(m.power_GB_in[k] == m.power_GG_buy[k] + m.power_EL_out_GB[k] + m.power_HT_out[k])
+    # =========================================================================
+    ## eq6: POWER LIMIT at ELECTROLYSER
+    # ==========================================================================
+    #m.constraints.add(pyomo.inequality(0.10*m.power_EL_rated_aux[k], m.power_EL_in[k],1*m.power_EL_rated_aux[k]))
+    # =========================================================================
+    ## eq7: EFFICIENCY at ELECTROLYSER
+    # ==========================================================================
+    m.constraints.add(m.power_EL_out[k] == glo.efficiency_ele*m.power_EL_in[k])
+    # =========================================================================
+    ## eq8: DELTA LINEARIZATION at ELECTROLYSER
+    # ==========================================================================
+    m.constraints.add(m.power_EL_rated_aux[k] == m.power_EL_rated[k]*m.optDELTA_ele[k])
+    # =========================================================================
+    ## eq9: LINEARIZATION condition
+    # ==========================================================================
+    m.constraints.add(m.power_EL_rated_aux[k] <= m.power_EL_rated[k]* - (1 - m.optDELTA_ele[k])*glo.power_EL_rated_min)
+    # =========================================================================
+    ## eq10: LINEARIZATION condition
+    # ==========================================================================
+    m.constraints.add(m.power_EL_rated_aux[k] >= m.power_EL_rated[k]* - (1 - m.optDELTA_ele[k])*glo.power_EL_rated_max)
+    # =========================================================================
+    ## eq11: LINEARIZATION condition
+    # ==========================================================================
+    m.constraints.add(m.power_EL_rated_aux[k] <= glo.power_EL_rated_max*m.optDELTA_ele[k])
+    # =========================================================================
+    ## eq12: LINEARIZATION condition
+    # ==========================================================================
+    m.constraints.add(m.power_EL_rated_aux[k] >= glo.power_EL_rated_min*m.optDELTA_ele[k])
+    # =========================================================================
+    ## eq13: POWER required by the COMPRESSOR
+    # ==========================================================================
+    m.constraints.add(m.power_CP_in[k] == m.power_EL_out_CP[k]*glo.compression_work / glo.LHV_h2)                  
+    # =========================================================================
+    ## eq14: POWER BALANCE pot of the electrolyser
+    # ==========================================================================
+    m.constraints.add(m.power_EL_out_CP[k] == m.power_HT_in[k])
+    # =========================================================================
+    ## eq15: HYDROGEN TANK ENERGY BALANCE
+    # ==========================================================================
+    m.constraints.add(m.capacity_HT[k+1] == m.capacity_HT[k] + m.power_HT_in[k] - m.power_HT_out[k])
+    # =========================================================================
+    ## eq16: HYDROGEN TANK Initial condition
+    # ==========================================================================
+    m.constraints.add(m.capacity_HT[0] == glo.capacity_HT_rated * 0.90)
+    # =========================================================================
+    ## eq18: ENERGY BALANCE in the STORAGE TANK : CAPACITY CONSTRAINT
+    # ==========================================================================
+    m.constraints.add(glo.perc_min_ht * glo.capacity_HT_rated <= m.capacity_HT[k])          # we have to consider the E[k+1] or E[k]?
+    m.constraints.add(m.capacity_HT[k] <= glo.perc_max_ht * glo.capacity_HT_rated)       
+    # =========================================================================
+    ## eq19: BURNER EFFICIENCY
+    # ==========================================================================
+    m.constraints.add(m.power_GB_out[k] == glo.efficiency_bur*m.power_GB_in[k])
+    # =========================================================================
+    ## eq20: BURNER BALANCE: constraint on POWER AT INLET
+    # ==========================================================================
+    m.constraints.add(glo.perc_min_bur*glo.power_bur_rated <= m.power_GB_in[k])
+    m.constraints.add(m.power_GB_in[k] <= glo.perc_max_bur*glo.power_bur_rated)
+    # =========================================================================
+    ## eq21: POWER BALANCE: LOAD CONSTRAINT
+    # ==========================================================================
+    m.constraints.add(m.power_GB_out[k] == m.list_load_furnace[k])
+    
+def obj_func(m):
+    return sum(m.power_EL_out[kk]*glo.CAPEX_ele for kk in m.t)
+    
+m.obj = pyomo.Objective(rule=obj_func, sense=pyomo.minimize)
+instance = m.create_instance()
+opt = pyomo.SolverFactory('gurobi')
+result = opt.solve(instance)
+result.write()
+
+
+
+# ===============================================================================================================================================
+## # STORE the values model.optimization
+# ===============================================================================================================================================
+pow_ele = []
+for ii in m.t:
+    pow_ele.append(pyomo.value(m.power_EL_out[ii]))
+        
+plt.plot(m.t, pow_ele)
+print('Result from OPTIMIZATION FUNCTION = ', m.obj())   
+
+    
+    
+              
+    
+    
+    
